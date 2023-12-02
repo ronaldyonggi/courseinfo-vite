abf719d7855a88a00d270b27571ef61f7fcdfd33
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-12-02 18:00:19 +0800

    2.3 sum of exercises calculated with reduce

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -1,10 +1,14 @@
 const Course = ({ course }) => {
+    const arr = course.parts
+    const total = arr.reduce((acc, curr) => acc + curr.exercises, 0)
     return (
         <>
             <h1>{course.name}</h1>
             {course.parts.map(p => (
                 <li key={p.id}>{p.name} {p.exercises}</li>
             ))}
+            <strong>total of {total} exercises</strong>
+
         </>
     )
 }
